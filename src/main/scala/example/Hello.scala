f4cbe02ddc0003f8c35a04a66ac136fbfcdc6f42
gjuoun <gjuoun@gmail.com>
2020-07-07 01:26:21 -0300

    Collection -> list

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,11 +1,42 @@
 package example
 
+import java.util.Date
+import Array._
+
 object Hello extends App {
-  var a = 0;
-  val numList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-  // for 循环
-  var loopVals = for { a <- numList if a != 3; if a < 8 } yield a
+  def log(date: Date, message: String) = {
+    println(date + "----" + message)
+  }
 
-  println(loopVals)
+  val logBind = log(new Date(), _: String)
+
+  logBind("hey 1")
+  logBind("hey 2")
+  logBind("hey 3")
+
+  val add2 = (x: Int) => x + 1
+  println(add2(5))
+
+  val add3 = new Function2[Int,Int, Int] {
+    def apply(x: Int, y: Int): Int = x + y
+  }
+
+
+  var myList1 = range(10, 20 ,1)
+  println(myList1.toList)
+
+  var myList2 = apply((3,4,5,6,7))
+  println(myList2.getClass())
+
+  var someList = Array(2,3,4,5,6,7)
+  val diff = someList.diff(Array(4,5,6,7,8))
+
+  println(diff.mkString(","))
+
+
+  val site1: List[String] = List("Runoob", "Google", "Baidu")
+  val site2 = "Runoob" :: ("Google" :: ("Baidu" :: Nil))
+
+  println( List.concat(site1, site2))
 }
